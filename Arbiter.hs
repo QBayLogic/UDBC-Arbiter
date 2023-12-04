50eabe966c8c085684982568944a5ca6f3e1d7e0
Felix Klein <felix@qbaylogic.com>
2023-12-04 16:14:27 +0100

    Add security features

--- Arbiter.hs
+++ Arbiter.hs
@@ -1,41 +1,127 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module: Arbiter
+-- Maintainer: QBayLogic B.V. <devops@qbaylogic.com>
+--
+-- A Clash arbiter implementation supporting a bounded number of
+-- clients and arbitrary (theoretically unbounded) data packages to be
+-- exchanged. Data packages additionally are classified according to
+-- LOW or HIGH security levels. The given implementation has no
+-- particular practical relevance, but is more of theoretical interest
+-- in term of safety and security property verification.
+--
+-----------------------------------------------------------------------------
+
 module Arbiter where
 
+-----------------------------------------------------------------------------
+
+import Data.Proxy
 import Clash.Prelude
 import Clash.Prelude.Testbench
 
-type Item    a = Maybe a
+-----------------------------------------------------------------------------
+
+-- | The input request type of the arbiter, as offered by clients.
+data Request a =
+    NoRq
+    -- ^ The client does not request anything.
+  | Lo a
+    -- ^ The client places a request with the given low data packet.
+  | Hi a
+    -- ^ The client places a request with the given high data packet.
+  deriving (Generic, NFDataX, Show)
+
+-- | The outputted grant type of the arbiter.
+data Grant n a =
+    NoRequest
+    -- ^ There is currently no open request to be served.
+  | ReqFrom (Index n) a
+    -- ^ The client with the given index has placed a request with the
+    -- attached data package.
+  | AccessDenied
+    -- ^ The security policy does not allow any insights here.
+  deriving (Generic, NFDataX, Show)
+
+-- | The security level, as used by the internal state.
+data SecurityLevel =
+    L -- ^ low security data
+  | H -- ^ high security data
+  deriving (Generic, NFDataX, Show)
+
+-- | An element of the store.
+type Item a = Maybe a
+
+-- | The internal state for holing the open requests and attached data
+-- packets.
 type Store n a = Vec n (Item a)
-type Grant n a = Maybe (Index n, a)
-type Request a = Maybe a
 
-udbcArbiter
-  :: forall dom n a. (KnownDomain dom, HiddenClockResetEnable dom, KnownNat n, NFDataX a)
-  => Signal dom (Vec n (Request a)) -> Signal dom (Grant n a)
-udbcArbiter requests = grant
-  where
-    grant :: Signal dom (Grant n a)
-    grant = register Nothing (next <$> store'' <*> grant)
+-- | A data packet with an attached security level.
+type Sec a = (a, SecurityLevel)
 
-    store :: Signal dom (Store n a)
-    store   = register (repeat Nothing) store''
-    store'  = rmGranted      <$> store  <*> grant
-    store'' = addNewRequests <$> store' <*> requests
+-----------------------------------------------------------------------------
 
-    addNewRequests :: Store n a -> Vec n (Request a) -> Store n a
-    addNewRequests s v = maybe id (const . Just) <$> v <*> s
+-- | The bounded client / unbounded data arbiter.
+udbcArbiter ::
+  forall dom n a.
+  (KnownDomain dom, HiddenClockResetEnable dom, KnownNat n, NFDataX a) =>
+  Signal dom (Vec n (Request a)) -> Signal dom (Grant n a, Grant n a)
+udbcArbiter requests = bundle
+  ( permitted L <$> grant -- LOW  security output channel
+  , permitted H <$> grant -- HIGH security output channel
+  )
+ where
+  -- restricts data access according to the given security level
+  permitted H (Just (i, (x, _))) = ReqFrom i x
+  permitted L (Just (i, (x, L))) = ReqFrom i x
+  permitted L (Just (_, (_, H))) = AccessDenied
+  permitted _ _                  = NoRequest
 
-    rmGranted :: Store n a -> Grant n a -> Store n a
-    rmGranted s = maybe s (\(i, _) -> replace i Nothing s)
+  -- the store for holding the still open requests
+  store0, store1, store2 :: Signal dom (Store n (Sec a))
+  store0 = register (repeat Nothing) store2
+  store1 = rmGranted      <$> store0 <*> prevSelected
+  store2 = addNewRequests <$> store1 <*> requests
 
-    next :: Store n a -> Grant n a -> Grant n a
-    next st cur =
-      let maxBy myMax = fold myMax (Nothing :> openRI st) in
-        (\i -> (i, let Just x = st !! i in x))
-           <$> (maxBy (maxLI (fst <$> cur)) <|> maxBy max)
+  -- the currently selected grant
+  grant =
+    (\st -> maybe Nothing (\i -> (i, ) <$> (st !! i)))
+      <$> store2 <*> selected
 
-    openRI :: Store n a -> Store n (Index n)
-    openRI st = maybe (const Nothing) (const Just) <$> st <*> indicesI
+  -- the currently and previously selected index to be granted
+  selected, prevSelected :: Signal dom (Maybe (Index n))
+  selected = next <$> store2 <*> prevSelected
+  prevSelected = register Nothing selected
 
+  -- adds the new requests of the current cycle to the store
+  addNewRequests :: Store n (Sec a) -> Vec n (Request a) -> Store n (Sec a)
+  addNewRequests st rs = add <$> rs <*> st
+   where
+    add NoRq   = id
+    add (Lo x) = const $ Just (x, L)
+    add (Hi x) = const $ Just (x, H)
+
+  -- removes the last granted element from the store
+  rmGranted :: Store n b -> Maybe (Index n) -> Store n b
+  rmGranted s = maybe s (\i -> replace i Nothing s)
+
+  -- determines the next index in the store to be selected using a
+  -- round robin strategy. The maximum index being smaller than the
+  -- given index is chosen, if it exists. Otherwise the maximum of all
+  -- elements is taken.
+  next :: Store n b -> Maybe (Index n) -> Maybe (Index n)
+  next st cur = maxBy (maxLI cur) <|> maxBy max
+   where
+    -- select the maximum of all elements according to the given
+    -- implementation
+    maxBy myMax = fold myMax (Nothing :> filledPositions)
+
+    -- all indices that reference non-empty data elements in the store
+    filledPositions =
+      maybe (const Nothing) (const Just) <$> st <*> indicesI
+
+    -- choose the maximal element among the second and third argument
+    -- that is strictly smaller than the first argument
     maxLI :: Ord b => Item b -> Item b -> Item b -> Item b
     maxLI cur a b
       | a < cur && b < cur = max a b
@@ -43,22 +129,35 @@ udbcArbiter requests = grant
       | b < cur            = b
       | otherwise          = Nothing
 
+-----------------------------------------------------------------------------
+
+-- | Some test input to see the UDBC-arbiter in action.
 testInput :: HiddenClockResetEnable dom => Signal dom (Vec 3 (Request Int))
 testInput = stimuliGenerator
-  (  (Nothing :> Nothing :> Nothing :> Nil)
-  :> (Just 1  :> Nothing :> Nothing :> Nil)
-  :> (Nothing :> Nothing :> Nothing :> Nil)
-  :> (Nothing :>  Just 2 :>  Just 3 :> Nil)
-  :> ( Just 4 :> Nothing :> Nothing :> Nil)
-  :> (Nothing :> Nothing :>  Just 3 :> Nil)
-  :> (Nothing :> Nothing :> Nothing :> Nil)
-  :> ( Just 5 :>  Just 6 :>  Just 7 :> Nil)
-  :> (Nothing :> Nothing :> Nothing :> Nil)
+  (  (NoRq :> NoRq :> NoRq :> Nil)
+  :> (Lo 1 :> NoRq :> NoRq :> Nil)
+  :> (NoRq :> NoRq :> NoRq :> Nil)
+  :> (NoRq :> Hi 2 :> Lo 3 :> Nil)
+  :> (Hi 4 :> NoRq :> NoRq :> Nil)
+  :> (NoRq :> NoRq :> Lo 3 :> Nil)
+  :> (NoRq :> NoRq :> NoRq :> Nil)
+  :> (Lo 5 :> Hi 6 :> Hi 7 :> Nil)
+  :> (NoRq :> NoRq :> NoRq :> Nil)
   :> Nil
   )
 
+-- | Simulates the UDBC Arbiter on 'testInput'.
 simulateArbiter :: IO ()
-simulateArbiter =
-  mapM_ print
-    $ sampleN @System 12
-    $ bundle (testInput, udbcArbiter testInput)
+simulateArbiter = do
+  mapM_ pr $ sampleN @System 12 $ bundle (testInput, udbcArbiter testInput)
+  putStrLn sep
+ where
+  pr (inp, (l, h)) = do
+    putStrLn sep
+    putStrLn $ "Inputs:   " <> show inp
+    putStrLn $ "Low  Out: " <> show l
+    putStrLn $ "High Out: " <> show h
+
+  sep = "--------------------------------------"
+
+-----------------------------------------------------------------------------
--- README.md
+++ README.md
@@ -1,10 +1,30 @@
-# UDBC-Arbiter
+# The Clash UDBC-Arbiter Verification Challenge
 
-An Unbounded Data / Bounded Client Arbiter implementid in Clash
+The repository offers arbiter an implementation in [Clash](https://clash-lang.org) for a bounded number of clients and arbitrary (theoretically unbounded) data packages to be exchanged. Data packages additionally are classified according to LOW or HIGH security levels distributed along two output channels with respective access control, i.e., the LOW output channel only has access to LOW security data, while the HIGH security output channel has access to all data that is returned by the arbiter.
+
+## What's the challenge?
+
+The challenge is to formally verify that the given implementation satisfies the following safety and security properties for any number of clients and all potential data types:
+
+* __Mutual Exclusion:__ _No more than one request is granted at a time._
+* __Productivity:__ _Every open request eventually gets granted._
+* __Fairness:__ _There exists an upper bound on the number of cycles, after which every request gets granted._
+* __Access Control:__ _Sensitive data never appears on the LOW output channel._
+* __Non-interference:__ _HIGH inputs cannot observably influence LOW outputs._
+* __Data Integrity:__ _The data attached to requests is the same as the data delivered with the corresponding grants._
+
+Note that clients are allowed to place new requests although their previous requests have not been granted yet, in which case the previous requests are automatically closed and replaced by the newest one. The above properties only consider open requests in that sense.
 
 ## Getting Started
 
+Load the example with the Clash interpreter:
+
 ```
 clashi Arbiter.hs
+```
+
+Simulate the arbiter on some inputs:
+
+```
 clashi> simulateArbiter
 ```
